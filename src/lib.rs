248b8a083f07360a5e41a9cdb00720974fc97553
Jannis R <mail@jannisr.de>
2018-05-09 17:13:36 +0200

    baby steps

--- src/lib.rs
+++ src/lib.rs
@@ -1,3 +1,5 @@
+#[macro_use] extern crate slugify;
+use slugify::slugify;
 use std::collections::HashMap;
 
 pub struct Index {
@@ -25,10 +27,26 @@ pub fn build_index(items: Vec<Item>) -> Index {
 
 	let mut id = 0;
 	for item in &items {
+		let tokens = slugify!(&item.name, separator = " ");
+		let tokens = tokens.split_whitespace();
+
+		let mut nr_of_tokens = 0;
+		for token in tokens {
+			let mut ids = index.tokens
+				.entry(token.to_string())
+				.or_insert(vec![]);
+			ids.push(id); // todo: check if added
+			nr_of_tokens += 1;
+		}
 		index.weights.push(item.weight);
+		index.nr_of_tokens.push(nr_of_tokens);
 		index.original_ids.push(item.id.clone());
-		// todo
 		id += 1;
+	}
+
+	for (token, ids) in index.tokens.iter() {
+		let score: f64 = ids.len() as f64 / items.len() as f64;
+		index.scores.insert(token.to_string(), score);
 	}
 
 	index
