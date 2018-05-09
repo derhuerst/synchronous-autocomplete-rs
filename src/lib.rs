c60d8161af26d78d24d2a87c9508a78756cdb5e0
Jannis R <mail@jannisr.de>
2018-05-09 16:27:01 +0200

    baby steps

--- src/lib.rs
+++ src/lib.rs
@@ -1,1 +1,35 @@
-// todo
+use std::collections::HashMap;
+
+pub struct Index {
+	pub tokens: HashMap<String, Vec<i32>>,
+	pub scores: HashMap<String, f64>,
+	pub weights: Vec<f64>,
+	pub nr_of_tokens: Vec<i32>,
+	pub original_ids: Vec<String>
+}
+
+pub struct Item {
+	pub id: String,
+	pub name: String,
+	pub weight: f64
+}
+
+pub fn build_index(items: Vec<Item>) -> Index {
+	let mut index = Index {
+		tokens: HashMap::new(),
+		scores: HashMap::new(),
+		weights: vec![],
+		nr_of_tokens: vec![],
+		original_ids: vec![]
+	};
+
+	let mut id = 0;
+	for item in &items {
+		index.weights.push(item.weight);
+		index.original_ids.push(item.id.clone());
+		// todo
+		id += 1;
+	}
+
+	index
+}
