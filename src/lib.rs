f6953c18be293e400dbbfb1704e4ff31344e26c2
Jannis R <mail@jannisr.de>
2018-05-11 12:16:51 +0200

    tests :white_check_mark:

--- src/lib.rs
+++ src/lib.rs
@@ -161,3 +161,207 @@ pub fn run(idx: &Index, query: String, completion: bool, fuzzy: bool) -> Vec<Res
 
 	items
 }
+
+#[cfg(test)]
+mod test {
+	use super::*;
+
+	fn sample_index() -> Index {
+		let mut tokens = HashMap::new();
+		tokens.insert(String::from("one"), vec![0]);
+		tokens.insert(String::from("two"), vec![1]);
+		tokens.insert(String::from("three"), vec![1]);
+		tokens.insert(String::from("four"), vec![0, 1]);
+
+		let mut scores = HashMap::new();
+		scores.insert(String::from("one"), 1.0 / 2.0);
+		scores.insert(String::from("two"), 1.0 / 2.0);
+		scores.insert(String::from("three"), 1.0 / 2.0);
+		scores.insert(String::from("four"), 1.0);
+
+		Index {
+			tokens,
+			scores,
+			weights: vec![10.0, 20.0],
+			nr_of_tokens: vec![2, 3],
+			original_ids: vec![String::from("A"), String::from("B")]
+		}
+	}
+
+	#[test]
+	fn build_index_works() {
+		let items = vec![
+			Item {
+				id: String::from("A"),
+				name: String::from("One fOUr!"),
+				weight: 10.0
+			},
+			Item {
+				id: String::from("B"),
+				name: String::from("two THREE four?"),
+				weight: 20.0
+			}
+		];
+		let exp = sample_index();
+		let act = build_index(items);
+
+		// deep_equal(act.tokens, exp.tokens)
+		assert_eq!(act.tokens.len(), exp.tokens.len());
+		for (token, exp_ids) in exp.tokens {
+			let act_ids = act.tokens.get(&token).expect("");
+			for i in 0..(exp_ids.len() - 1) {
+				assert_eq!(act_ids.get(i), exp_ids.get(i));
+			}
+		}
+
+		// deep_equal(act.scores, exp.scores)
+		assert_eq!(act.scores.len(), exp.scores.len());
+		for (token, exp_score) in exp.scores {
+			let act_score = act.scores.get(&token).expect("");
+			assert_eq!(*act_score, exp_score);
+		}
+
+		// deep_equal(act.weights, exp.weights)
+		assert_eq!(act.weights.len(), exp.weights.len());
+		for i in 0..(exp.weights.len() - 1) {
+			let act_weight = act.weights.get(i).expect("");
+			let exp_weight = exp.weights.get(i).expect("");
+			assert_eq!(act_weight, exp_weight);
+		}
+
+		// deep_equal(act.nr_of_tokens, exp.nr_of_tokens)
+		assert_eq!(act.nr_of_tokens.len(), exp.nr_of_tokens.len());
+		for i in 0..(exp.nr_of_tokens.len() - 1) {
+			let act_nr = act.nr_of_tokens.get(i).expect("");
+			let exp_nr = exp.nr_of_tokens.get(i).expect("");
+			assert_eq!(act_nr, exp_nr);
+		}
+
+		// deep_equal(act.original_ids, exp.original_ids)
+		assert_eq!(act.original_ids.len(), exp.original_ids.len());
+		for i in 0..(exp.original_ids.len() - 1) {
+			let act_id = act.original_ids.get(i).expect("");
+			let exp_id = exp.original_ids.get(i).expect("");
+			assert_eq!(act_id, exp_id);
+		}
+	}
+
+	#[test]
+	fn build_index_handles_duplicate_tokens() {
+		let items = vec![
+			Item {
+				id: String::from("A"),
+				name: String::from("foo bar foo"), // "foo" twice
+				weight: 10.0
+			},
+			Item {
+				id: String::from("B"),
+				name: String::from("foo baz"),
+				weight: 20.0
+			}
+		];
+		let idx = build_index(items);
+
+		let foo = idx.tokens.get("foo").expect("missing foo IDs");
+		assert_eq!(foo.len(), 3);
+		assert_eq!(*foo.get(0).expect("missing foo[0]"), 0);
+		assert_eq!(*foo.get(1).expect("missing foo[1]"), 0);
+		assert_eq!(*foo.get(2).expect("missing foo[2]"), 1);
+
+		let nrs = idx.nr_of_tokens;
+		assert_eq!(nrs.len(), 2);
+		assert_eq!(*nrs.get(0).expect("missing idx.nr_of_tokens[0]"), 3);
+		assert_eq!(*nrs.get(1).expect("missing idx.nr_of_tokens[1]"), 2);
+	}
+
+	#[test]
+	fn with_fragment_exact() {
+		let idx = sample_index();
+		let res = with_fragment(&idx, String::from("four"), false, false);
+
+		assert_eq!(res.len(), 2);
+		// 1 + scores[fragment] + sqrt(fragment_length)
+		assert_eq!(*res.get(&0).expect(""), 1.0 + 1.0 + 2.0);
+		assert_eq!(*res.get(&1).expect(""), 1.0 + 1.0 + 2.0);
+	}
+
+	#[test]
+	fn with_fragment_completion() {
+		let idx = sample_index();
+		let res = with_fragment(&idx, String::from("fou"), true, false);
+
+		assert_eq!(res.len(), 2);
+		// 1 + scores[fragment] + fragment_length / token_length
+		let a = res.get(&0).expect("missing result for A");
+		assert_eq!(*a, 1.0 + 1.0 + 3.0 / 4.0);
+		let b = res.get(&1).expect("missing result for B");
+		assert_eq!(*b, 1.0 + 1.0 + 3.0 / 4.0);
+	}
+
+	#[test]
+	fn with_fragment_fuzzy() {
+		let idx = sample_index();
+		let res = with_fragment(&idx, String::from("there"), false, true);
+
+		assert_eq!(res.len(), 1);
+		// (1 + scores[fragment]) / (1 + levenshtein_distance)
+		let b = res.get(&1).expect("missing result for B");
+		assert_eq!(*b, (1.0 + 0.5) / (1.0 + levenshtein("there", "three") as f64));
+	}
+
+	#[test]
+	fn run_calculates_correctly() {
+		let idx = sample_index();
+		let res = run(&idx, String::from("fou"), true, false);
+
+		assert_eq!(res.len(), 2);
+		let a = res.get(0).expect("missing 1st result");
+		let b = res.get(1).expect("missing 2nd result");
+
+		// (1 + scores[token] + fragment_length / token_length) / nr_of_tokens[id]
+		let a_rel = (1.0 + 1.0 + 3.0 / 4.0) / 2.0;
+		let b_rel = (1.0 + 1.0 + 3.0 / 4.0) / 3.0;
+		// relevance * pow(weights[id], 1 / 3)
+		let a_score = a_rel * (10.0 as f64).powf(1.0 / 3.0);
+		let b_score = b_rel * (20.0 as f64).powf(1.0 / 3.0);
+
+		assert_eq!(a.id, String::from("A"));
+		assert_eq!(a.relevance, a_rel);
+		assert_eq!(a.score, a_score);
+
+		assert_eq!(b.id, String::from("B"));
+		assert_eq!(b.relevance, b_rel);
+		assert_eq!(b.score, b_score);
+	}
+
+	#[test]
+	fn run_handles_duplicate_tokens() {
+		// A: "foo bar foo", weight 10
+		// B: "foo baz", weight 20
+		let mut tokens = HashMap::new();
+		tokens.insert(String::from("foo"), vec![0, 0, 1]);
+		tokens.insert(String::from("bar"), vec![0]);
+		tokens.insert(String::from("baz"), vec![1]);
+		let mut scores = HashMap::new();
+		scores.insert(String::from("foo"), 2.0 / 2.0);
+		scores.insert(String::from("bar"), 1.0 / 2.0);
+		scores.insert(String::from("baz"), 1.0 / 2.0);
+		let idx = Index {
+			tokens,
+			scores,
+			weights: vec![10.0, 20.0],
+			nr_of_tokens: vec![3, 2],
+			original_ids: vec![String::from("A"), String::from("B")]
+		};
+
+		// For the query "foo ba", B should rank higher, except if the duplicate
+		// token "foo" in A is taken into account. We assert that it is.
+		let res = run(&idx, String::from("foo ba"), true, false);
+		let a = res.get(0).expect("missing 1st result");
+		let b = res.get(1).expect("missing 2nd result");
+		assert_eq!(a.id, String::from("A"));
+		assert_eq!(b.id, String::from("B"));
+	}
+
+	// todo: run_limits_nr_of_results
+}
